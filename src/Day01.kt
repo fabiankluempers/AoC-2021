31b5e1dad6d67a67ce09a960bfc6785429756251
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-12-03 17:17:38 +0100

    Refactor Day01 and Day02 so that they are classes and extend Puzzle

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,13 @@
-fun main() {
-	fun part1(input: List<Int>) = input
+class Day01 : Puzzle("Day01", 7, 5) {
+	override fun part1(input: List<String>) = input
+		.map(String::toInt)
 		.zipWithNext()
 		.count { it.second > it.first }
 
-	fun part2(input: List<Int>) = input
+	override fun part2(input: List<String>) = input
+		.map(String::toInt)
 		.windowed(3, 1)
 		.map(List<Int>::sum)
 		.zipWithNext()
 		.count { it.second > it.first }
-
-	// test if implementation meets criteria from the description, like:
-	val testInput = readInput("Day01_test").map(String::toInt)
-	check(part1(testInput) == 7)
-	check(part2(testInput) == 5)
-
-	val input = readInput("Day01").map(String::toInt)
-	println(part1(input))
-	println(part2(input))
 }
--- src/Day02.kt
+++ src/Day02.kt
@@ -1,22 +1,12 @@
-fun main() {
-	fun Pair<Int, Int>.plus(pair: Pair<Int, Int>): Pair<Int, Int> =
-		Pair(this.first + pair.first, this.second + pair.second)
-
-	fun String.toPos(delta: Int): Pair<Int, Int> = when (this) {
-		"forward" -> Pair(delta, 0)
-		"down" -> Pair(0, delta)
-		"up" -> Pair(0, -delta)
-		else -> throw IllegalArgumentException()
-	}
-
-	fun part1(input: List<String>) = input
+class Day02 : Puzzle("Day02", 150, 900) {
+	override fun part1(input: List<String>) = input
 		.map { it.split(' ') }
 		.map { it[0].toPos(it[1].toInt()) }
-		.fold(Pair(0, 0), Pair<Int, Int>::plus)
+		.fold(Pair(0, 0)) { acc, pair -> acc + pair }
 		.let { it.first * it.second }
 
-	fun part2(input: List<String>) =
-		input.fold(Triple(0, 0, 0)) { (horizontal, vertical, aim), string ->
+	override fun part2(input: List<String>)= input
+		.fold(Triple(0, 0, 0)) { (horizontal, vertical, aim), string ->
 			val (direction, delta) = string.split(' ').let { Pair(it[0], it[1].toInt()) }
 			when (direction) {
 				"forward" -> Triple(horizontal + delta, vertical + (aim * delta), aim)
@@ -26,13 +16,14 @@ fun main() {
 			}
 		}.let { it.first * it.second }
 
-	// test if implementation meets criteria from the description, like:
-	val testInput = readInput("Day02_test")
-	check(part1(testInput) == 150)
-	check(part2(testInput) == 900)
+	private operator fun Pair<Int, Int>.plus(pair: Pair<Int, Int>): Pair<Int, Int> =
+		Pair(this.first + pair.first, this.second + pair.second)
 
-	val input = readInput("Day02")
-	println(part1(input))
-	println(part2(input))
+	private fun String.toPos(delta: Int): Pair<Int, Int> = when (this) {
+		"forward" -> Pair(delta, 0)
+		"down" -> Pair(0, delta)
+		"up" -> Pair(0, -delta)
+		else -> throw IllegalArgumentException()
+	}
 }
 
