21c2046f86b6d86dcae67dbcee97f81d5d4738ee
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-12-03 15:07:23 +0100

    Replace first solution for Day03 with more efficient iterative row-wise solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,35 +1,23 @@
+import kotlin.system.measureTimeMillis
 import kotlin.text.StringBuilder
 
 class Day03 : Puzzle("Day03", 198, 0) {
 	override fun part1(input: List<String>): Int {
-		val gamma = StringBuilder().apply {
-			input.toColumns().forEach { column ->
-				val zeroes = column.count { it == '0' }
-				val ones = column.length - zeroes
-				if (zeroes > ones) this.append('0') else this.append('1')
+		val count = Array(input[0].length) { 0 }
+		input.forEach {
+			for (i in count.indices) {
+				count[i] += if (it[i] == '1') 1 else -1
 			}
-		}.toString()
-		val epsilon = StringBuilder().apply {
-			gamma.forEach {
-				when (it) {
-					'0' -> this.append('1')
-					'1' -> this.append('0')
-					else -> error("cant flip $it")
-				}
-			}
-		}.toString()
+		}
+		val gammaBuilder = StringBuilder()
+		val epsilonBuilder = StringBuilder()
+		count.forEach {
+			gammaBuilder.append(if (it > 0) '1' else '0')
+			epsilonBuilder.append(if (it > 0) '0' else '1')
+		}
+		val gamma = gammaBuilder.toString()
+		val epsilon = epsilonBuilder.toString()
 		return gamma.toInt(radix = 2) * epsilon.toInt(radix = 2)
-	}
-
-	private fun List<String>.toColumns(): List<String> {
-		val columns = List(this[0].length) { StringBuilder() }
-		return this.map { it.toCharArray() }.fold(columns) { acc, chars ->
-			acc.also {
-				chars.forEachIndexed { index, char ->
-					it[index].append(char)
-				}
-			}
-		}.map(StringBuilder::toString)
 	}
 
 	override fun part2(input: List<String>): Int {
