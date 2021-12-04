b5964dcef61e6dc8e7731b3cc4dfab6e52d1b21e
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-12-04 14:51:31 +0100

    Refactor Day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,34 +1,20 @@
 class Day04 : Puzzle("Day04", 4512 , 1924) {
-	override fun part1(input: Input): Int {
-		val drawnNumbers = input.getDrawnNumbers()
-		val boards = input.getBoards()
-
-		for (i in drawnNumbers.indices) {
-			boards.forEach { board ->
-				if (board.markAndIsWin(drawnNumbers[i])) {
-					return board.calculateFinalScore(drawnNumbers[i])
-				}
-			}
-		}
-		error("Could not determine a Winner")
-	}
-
 	private fun Input.getDrawnNumbers() = this[0].trim().split(',').map(String::toInt)
 
 	private fun Input.getBoards() = this.drop(2).filter(String::isNotBlank).chunked(5).map {  it.toBoard() }
 
-	private fun List<String>.toBoard() : Array<Array<Pair<Int, Boolean>>> = this.map { line ->
+	private fun Input.toBoard() : BingoBoard = this.map { line ->
 		line.trim().split(Regex("""\s+""")).map { Pair(it.toInt(), false) }.toTypedArray()
 	}.toTypedArray()
 
-	private fun Array<Array<Pair<Int, Boolean>>>.markAndIsWin(drawnNumber : Int) : Boolean {
+	private fun BingoBoard.markAndIsWin(drawnNumber : Int) : Boolean {
 		for (x in this.indices) {
 			for (y in this[x].indices) {
 				val cell = this[x][y]
 				if (cell.first == drawnNumber) {
 					this[x][y] = cell.copy(second = true)
-					val checkRow = (this[x].all { it.second })
-					val checkColumn = this.map { it[y] }.all { it.second }
+					val checkRow = this[x].all { it.second }
+					val checkColumn = this.all { it[y].second }
 					return checkRow || checkColumn
 				}
 			}
@@ -36,19 +22,39 @@ class Day04 : Puzzle("Day04", 4512 , 1924) {
 		return false
 	}
 
-	private fun Array<Array<Pair<Int, Boolean>>>.calculateFinalScore(winningNumber: Int) =
-		this.flatMap { row -> row.filter { !it.second } }.sumOf(Pair<Int, Boolean>::first) * winningNumber
+	private fun BingoBoard.sumOfUnmarked() =
+		this.flatMap { row -> row.filter { !it.second } }.sumOf(Pair<Int, Boolean>::first)
+
+	override fun part1(input: Input): Int {
+		val drawnNumbers = input.getDrawnNumbers()
+		val boards = input.getBoards()
+
+		for (i in drawnNumbers.indices) {
+			boards.forEach { board ->
+				if (board.markAndIsWin(drawnNumbers[i])) {
+					return board.sumOfUnmarked() * drawnNumbers[i]
+				}
+			}
+		}
+
+		error("Could not determine a Winner")
+	}
 
 	override fun part2(input: Input): Int {
 		val drawnNumbers = input.getDrawnNumbers()
 		val boards = input.getBoards()
 		val candidates = boards.toMutableList()
 		var drawnNumberIndex = 0
+
 		while (candidates.size > 1) {
 			candidates.removeIf { it.markAndIsWin(drawnNumbers[drawnNumberIndex]) }
 			drawnNumberIndex++
 		}
+
 		while (!candidates[0].markAndIsWin(drawnNumbers[drawnNumberIndex])) drawnNumberIndex++
-		return candidates[0].calculateFinalScore(drawnNumbers[drawnNumberIndex])
+
+		return candidates[0].sumOfUnmarked() * drawnNumbers[drawnNumberIndex]
 	}
 }
+
+typealias BingoBoard = Array<Array<Pair<Int, Boolean>>>
