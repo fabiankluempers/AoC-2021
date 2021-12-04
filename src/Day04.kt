6b459ec55da31f1a5de4e82e8558c9854a526ecb
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-12-04 15:12:51 +0100

    Remove redundant trim() call in Day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,5 +1,5 @@
 class Day04 : Puzzle("Day04", 4512 , 1924) {
-	private fun Input.getDrawnNumbers() = this[0].trim().split(',').map(String::toInt)
+	private fun Input.getDrawnNumbers() = this[0].split(',').map(String::toInt)
 
 	private fun Input.getBoards() = this.drop(2).filter(String::isNotBlank).chunked(5).map {  it.toBoard() }
 
