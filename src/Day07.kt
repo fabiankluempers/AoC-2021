97ee1f68d5835304745716f9b191623d72673440
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-12-07 12:04:23 +0100

    Refactor Day07 for performance improvement

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,16 +1,21 @@
 import kotlin.math.absoluteValue
 
 class Day07 : Puzzle<Int>("Day07", 37 , 168) {
-	override fun part1(input: Input): Int = solution(input) { x, y -> (x - y).absoluteValue }
+	override fun part1(input: Input): Int = solution(input, false)
 
-	override fun part2(input: Input): Int = solution(input) { x, y -> (x - y).absoluteValue.toTriangularNumber() }
+	override fun part2(input: Input): Int = solution(input, true)
 
-	private fun solution(input: Input, distanceFunc: (Int, Int) -> Int) : Int {
+	private fun solution(input: Input, isToTriangularNumber: Boolean) : Int {
 		val crabPositions = input.first().splitToInt(',')
 		return (0 .. crabPositions.maxOf { it }).mapIndexed { index, _ ->
-			crabPositions.sumOf { distanceFunc(it, index) }
+			crabPositions.sumOf {
+				with((it - index).absoluteValue) { if (isToTriangularNumber) toTriangularNumber() else this }
+			}
 		}.minOf { it }
 	}
 
 	private fun Int.toTriangularNumber() = (this * this + this) / 2
 }
+
+
+
