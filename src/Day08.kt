1e82f38160c002b3c2d0a5c6b85aa73b538e1101
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-12-08 21:49:01 +0100

    Refactor Day08

--- src/Day08.kt
+++ src/Day08.kt
@@ -7,48 +7,46 @@ class Day08 : Puzzle<Int>("Day08", 26, 61229) {
 
 	override fun part2(input: Input): Int = input.sumOf(::solveLine)
 
-	//This works but it is really not pretty. I might optimize, if i find time.
 	private fun solveLine(line: String): Int {
-		val intToSignal = mutableMapOf<Int, Set<Char>>()
-		val (input, output) = with(line.split(" | ")) {
-			Pair(this[0].split(' '), this[1].split(' '))
-		}
-		val inputByLength = input.groupBy { it.length }.mapValues { it.value.map(String::toSet).toMutableList() }
+		val digitToSignal = mutableMapOf<Int, Set<Char>>()
 
+		val (input, output) = line
+			.split(" | ")
+			.map { it.split(' ').map(String::toSet) }
+
+		val signalsByLength = input
+			.groupBy(Set<Char>::size)
+			.mapValues { it.value.toMutableList() }
+
+		// Map trivial digits.
 		listOf(1, 7, 4, 8).forEachIndexed { index: Int, i: Int ->
-			intToSignal[i] = inputByLength[uniqueLengths[index]]!!.first()
+			digitToSignal[i] = signalsByLength[uniqueLengths[index]]!!.first()
 		}
 
+		// Deduces the signal for the digit, adds it to digitToSignal map and removes it from the receiver.
 		fun MutableList<Set<Char>>.deduce(digit: Int, usingDigit: Int) {
-			val signalToUse = intToSignal[usingDigit]!!
-			this.find { if (it.size < signalToUse.size) signalToUse.containsAll(it) else it.containsAll(signalToUse) }
-				.also {
-					requireNotNull(it)
-					intToSignal[digit] = it
-					this.remove(it)
-				}
+			val signal = digitToSignal[usingDigit]!!
+			find { if (it.size < signal.size) signal.containsAll(it) else it.containsAll(signal) }.also {
+				requireNotNull(it)
+				digitToSignal[digit] = it
+				remove(it)
+			}
 		}
 
-		//deduce all digits of length 6
-		val inputLength6 = inputByLength[6]!!
-		//deduce 9
-		inputLength6.deduce(9, 4)
-		//deduce 0
-		inputLength6.deduce(0, 7)
-		//deduce 6
-		intToSignal[6] = inputLength6.first()
+		// Deduce all digits of length 6.
+		val candidatesOfLength6 = signalsByLength[6]!!
+		candidatesOfLength6.deduce(9, 4)
+		candidatesOfLength6.deduce(0, 7)
+		digitToSignal[6] = candidatesOfLength6.first()
 
-		//deduce all digits of length 5
-		val inputLength5 = inputByLength[5]!!
-		//deduce 3
-		inputLength5.deduce(3, 1)
-		//deduce 5
-		inputLength5.deduce(5, 6)
-		//deduce 2
-		intToSignal[2] = inputLength5.first()
+		// Deduce all digits of length 5.
+		val candidatesOfLength5 = signalsByLength[5]!!
+		candidatesOfLength5.deduce(3, 1)
+		candidatesOfLength5.deduce(5, 6)
+		digitToSignal[2] = candidatesOfLength5.first()
 
-		val signalToString = intToSignal.map { Pair(it.value, it.key.toString()) }.toMap()
+		val signalToString = digitToSignal.map { Pair(it.value, it.key.toString()) }.toMap()
 
-		return output.joinToString(separator = "") { signalToString[it.toSet()]!! }.toInt()
+		return output.joinToString(separator = "") { signalToString[it]!! }.toInt()
 	}
 }
