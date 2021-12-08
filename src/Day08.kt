61f011176c5c17e88104c9b56de94ff1c4c2d1a1
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-12-08 16:54:46 +0100

    Add solution for Day08

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,4 +1,7 @@
-class Day08 : Puzzle<Int>("Day08", 26, 0) {
+import java.lang.StringBuilder
+import javax.swing.text.ChangedCharSetException
+
+class Day08 : Puzzle<Int>("Day08", 26, 61229) {
 
 	//This is not very useful for solving the actual problem but it solves part1
 	override fun part1(input: Input): Int {
@@ -15,7 +18,54 @@ class Day08 : Puzzle<Int>("Day08", 26, 0) {
 		).sum()
 	}
 
-	override fun part2(input: Input): Int {
-		return 0
+	override fun part2(input: Input): Int = input.sumOf(::solveLine)
+
+	//This works but it is really not pretty. I might optimize, if i find time.
+	private fun solveLine(line: String) : Int {
+		val intToSignal = mutableMapOf<Int, Set<Char>>()
+		val (input, output) = with(line.split('|')) {
+			Pair(this[0].trim().split(' '), this[1].trim().split(' '))
+		}
+		val inputByLength = input.groupBy { it.length }.mapValues { it.value.map(String::toSet).toMutableList() }
+		intToSignal[1] = inputByLength[2]!!.first().toSet()
+		intToSignal[4] = inputByLength[4]!!.first().toSet()
+		intToSignal[7] = inputByLength[3]!!.first().toSet()
+		intToSignal[8] = inputByLength[7]!!.first().toSet()
+
+		fun MutableList<Set<Char>>.deduce(numToDeduce: Int, numToUse: Int) {
+			this.find {
+				it.containsAll(intToSignal[numToUse]!!)
+			}.also {
+				requireNotNull(it)
+				intToSignal[numToDeduce] = it
+				this.remove(it)
+			}
+		}
+
+		//deduce all digits of length 6
+		val inputLength6 = inputByLength[6]!!
+		//deduce 9
+		inputLength6.deduce(9, 4)
+		//deduce 0
+		inputLength6.deduce(0, 7)
+		//deduce 6
+		intToSignal[6] = inputLength6.first()
+
+		//deduce all digits of length 5
+		val inputLength5 = inputByLength[5]!!
+		//deduce 3
+		inputLength5.deduce(3, 1)
+		//deduce 5
+		inputLength5.find { intToSignal[6]!!.containsAll(it) }.also {
+			requireNotNull(it)
+			intToSignal[5] = it
+			inputLength5.remove(it)
+		}
+		//deduce 2
+		intToSignal[2] = inputLength5.first()
+
+		val signalToString = intToSignal.map { Pair(it.value, it.key.toString()) }.toMap()
+
+		return output.joinToString { signalToString[it.toSet()]!! }.toInt()
 	}
 }
