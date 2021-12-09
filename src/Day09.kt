4f546440542968895512aa8cd56e0bf4fbd497a6
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-12-09 22:59:54 +0100

    Add solution for Day09

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,15 +1,20 @@
-class Day09 : Puzzle<Int>("Day09", 15, 0) {
+class Day09 : Puzzle<Int>("Day09", 15, 1134) {
 	override fun part1(input: Input): Int {
 		val caveData = input.map { it.map(Char::digitToInt) }
-		val lowPoints = mutableListOf<Int>()
+		val lowPoints = caveData.lowPoints()
+		return lowPoints.sumOf { caveData[it.first][it.second] + 1 }
+	}
 
-		for (x in caveData.indices) {
-			for (y in caveData[x].indices) {
-				if (caveData.isLowPoint(x, y)) lowPoints.add(caveData[x][y])
+	private fun List<List<Int>>.lowPoints(): List<Pair<Int, Int>> {
+		val lowPoints = mutableListOf<Pair<Int, Int>>()
+
+		for (x in this.indices) {
+			for (y in this[x].indices) {
+				if (this.isLowPoint(x, y)) lowPoints.add(Pair(x, y))
 			}
 		}
 
-		return lowPoints.sumOf { it + 1 }
+		return lowPoints
 	}
 
 	private fun List<List<Int>>.isLowPoint(x: Int, y: Int): Boolean = listOfNotNull(
@@ -23,6 +28,29 @@ class Day09 : Puzzle<Int>("Day09", 15, 0) {
 		if (x in this.indices && y in this[x].indices) this[x][y] else null
 
 	override fun part2(input: Input): Int {
-		return 0
+		val caveData = input.map { it.map(Char::digitToInt) }
+		val lowPoints = caveData.lowPoints()
+		val nullableCaveData = Array(caveData.size) {
+			caveData[it].toTypedArray<Int?>()
+		}
+		val basinSizes = mutableListOf<Int>()
+		for (lowPoint in lowPoints) {
+			basinSizes += nullableCaveData.discoverBasin(lowPoint.first, lowPoint.second)
+		}
+		return basinSizes.sorted().takeLast(3).reduce { x, y -> x * y }
 	}
+
+	private fun Array<Array<Int?>>.discoverBasin(x: Int, y: Int): Int =
+		if (x !in this.indices || y !in this[x].indices || this[x][y] == null || this[x][y] == 9 )
+			0
+		else {
+			this[x][y] = null
+			listOf(
+				discoverBasin((x + 1), y),
+				discoverBasin(x, (y - 1)),
+				discoverBasin(x, (y + 1)),
+				discoverBasin((x - 1), y),
+			).sum() + 1
+		}
+
 }
