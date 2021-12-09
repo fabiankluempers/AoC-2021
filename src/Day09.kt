d2c7a09e0734da6e02d6fff9d11250b482bcd7c5
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-12-09 12:20:12 +0100

    Refactor Day09

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,24 +1,23 @@
 class Day09 : Puzzle<Int>("Day09", 15, 0) {
 	override fun part1(input: Input): Int {
 		val caveData = input.map { it.map(Char::digitToInt) }
-
-		fun isLowPoint(x: Int, y: Int): Boolean = listOfNotNull(
-			caveData.safeAccess((x + 1), y),
-			caveData.safeAccess((x - 1), y),
-			caveData.safeAccess(x, (y + 1)),
-			caveData.safeAccess(x, (y - 1)),
-		).all { it > caveData[x][y] }
-
 		val lowPoints = mutableListOf<Int>()
 
 		for (x in caveData.indices) {
 			for (y in caveData[x].indices) {
-				if (isLowPoint(x, y)) lowPoints.add(caveData[x][y])
+				if (caveData.isLowPoint(x, y)) lowPoints.add(caveData[x][y])
 			}
 		}
 
 		return lowPoints.sumOf { it + 1 }
 	}
+
+	private fun List<List<Int>>.isLowPoint(x: Int, y: Int): Boolean = listOfNotNull(
+		safeAccess((x + 1), y),
+		safeAccess((x - 1), y),
+		safeAccess(x, (y + 1)),
+		safeAccess(x, (y - 1)),
+	).all { it > this[x][y] }
 
 	private fun List<List<Int>>.safeAccess(x: Int, y: Int): Int? =
 		if (x in this.indices && y in this[x].indices) this[x][y] else null
