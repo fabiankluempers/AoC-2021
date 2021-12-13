db1f2d2b58a1fea119fd471016ead139d58d2dca
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-12-13 18:09:25 +0100

    Refactor Day13

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,5 +1,34 @@
 class Day13 : Puzzle<Int>("Day13", 17, 0) {
-	override fun part1(input: Input): Int = foldInputSheet(input, input.generateInstructions().subList(0, 1)).count()
+	sealed class FoldInstruction {
+		abstract fun foldAccordingly(points: MutableSet<Point>)
+	}
+
+	data class FoldVer(val index: Int) : FoldInstruction() {
+		override fun foldAccordingly(points: MutableSet<Point>) {
+			for (point in points.filter { it.x > this.index }) {
+				points.remove(point)
+				points.add(point.copy(x = (2 * this.index - point.x)))
+			}
+		}
+	}
+
+	data class FoldHor(val index: Int) : FoldInstruction() {
+		override fun foldAccordingly(points: MutableSet<Point>) {
+			for (point in points.filter { it.y > this.index }) {
+				points.remove(point)
+				points.add(point.copy(y = (2 * this.index - point.y)))
+			}
+		}
+	}
+
+	private fun parseFoldInstruction(string: String): FoldInstruction {
+		val (left, right) = string.split('=')
+		return when (left.last()) {
+			'x' -> FoldVer(right.toInt())
+			'y' -> FoldHor(right.toInt())
+			else -> error("could not read fold instruction $left")
+		}
+	}
 
 	private fun Input.generateInstructions() = this
 		.takeLastWhile { it.isNotBlank() }
@@ -13,44 +42,15 @@ class Day13 : Puzzle<Int>("Day13", 17, 0) {
 		return points
 	}
 
-	sealed class FoldInstruction {
-		abstract fun foldAccordingly(points: MutableSet<Point>)
-	}
-
-	data class FoldX(val index: Int) : FoldInstruction() {
-		override fun foldAccordingly(points: MutableSet<Point>) {
-			for (point in points.filter { it.x > this.index }) {
-				points.remove(point)
-				points.add(point.copy(x = (2 * this.index - point.x)))
-			}
-		}
-	}
-
-	data class FoldY(val index: Int) : FoldInstruction() {
-		override fun foldAccordingly(points: MutableSet<Point>) {
-			for (point in points.filter { it.y > this.index }) {
-				points.remove(point)
-				points.add(point.copy(y = (2 * this.index - point.y)))
-			}
-		}
-	}
-
-	private fun parseFoldInstruction(string: String): FoldInstruction {
-		val (left, right) = string.split('=')
-		return when (left.last()) {
-			'x' -> FoldX(right.toInt())
-			'y' -> FoldY(right.toInt())
-			else -> error("could not read fold instruction")
-		}
-	}
+	override fun part1(input: Input): Int = foldInputSheet(input, input.generateInstructions().subList(0, 1)).count()
 
 	override fun part2(input: Input): Int {
 		val points = foldInputSheet(input, input.generateInstructions())
 		val maxX = points.maxOf { it.x }
 		val maxY = points.maxOf { it.y }
-		val result = Array(maxY + 1) { Array(maxX + 1) { '.' } }
+		val result = Array(maxY + 1) { Array(maxX + 1) { "  " } }
 		for (point in points) {
-			result[point.y][point.x] = '#'
+			result[point.y][point.x] = "||"
 		}
 		val resultString = StringBuilder()
 		resultString.appendLine()
