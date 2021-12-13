bbef34238ee68e6bd8808f317552da0d9f2865f8
Fabian Klümpers <fabian.kluempers@udo.edu>
2021-12-13 16:31:28 +0100

    Add solution for Day13

---
+++ src/Day13.kt
@@ -0,0 +1,67 @@
+class Day13 : Puzzle<Int>("Day13", 17, 0) {
+	override fun part1(input: Input): Int = foldInputSheet(input, input.generateInstructions().subList(0, 1)).count()
+
+	private fun Input.generateInstructions() = this
+		.takeLastWhile { it.isNotBlank() }
+		.map { readFoldInstruction(it) }
+
+	private fun foldInputSheet(input: Input, instructions: List<FoldInstruction>): Set<Point> {
+		val points = input
+			.takeWhile { it.isNotBlank() }
+			.map { with(it.splitToInt(',')) { Point(x = component1(), y = component2()) } }.toMutableSet()
+		for (instruction in instructions) {
+			when (instruction) {
+				is FoldX -> {
+					for (point in points.filter { it.x > instruction.index }) {
+						val dx = 2 * instruction.index - point.x
+						points.remove(point)
+						points.add(point.copy(x = dx))
+					}
+				}
+				is FoldY -> {
+					for (point in points.filter { it.y > instruction.index }) {
+						val dy = 2 * instruction.index - point.y
+						points.remove(point)
+						points.add(point.copy(y = dy))
+					}
+				}
+			}
+		}
+		return points
+	}
+
+	sealed class FoldInstruction()
+
+	data class FoldX(val index: Int) : FoldInstruction()
+
+	data class FoldY(val index: Int) : FoldInstruction()
+
+	private fun readFoldInstruction(string: String): FoldInstruction {
+		val (left, right) = string.split('=')
+		return when (left.last()) {
+			'x' -> FoldX(right.toInt())
+			'y' -> FoldY(right.toInt())
+			else -> error("could not read fold instruction")
+		}
+	}
+
+	override fun part2(input: Input): Int {
+		val points = foldInputSheet(input, input.generateInstructions())
+		val maxX = points.maxOf { it.x }
+		val maxY = points.maxOf { it.y }
+		val result = Array(maxY + 1) { Array(maxX + 1) { '.' } }
+		for (point in points) {
+			result[point.y][point.x] = '#'
+		}
+		val resultString = StringBuilder()
+		resultString.appendLine()
+		for (y in result.indices) {
+			for (x in result[y].indices) {
+				resultString.append(result[y][x])
+			}
+			resultString.appendLine()
+		}
+		println(resultString)
+		return 0
+	}
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,31 @@
 import kotlin.system.measureTimeMillis
 
 fun main() {
-	val puzzle = Day12()
+	val puzzle = Day13()
 	val testInput = readInput("${puzzle.name}_test")
-	check(puzzle.part1(testInput) == puzzle.testResultPart1)
-	check(puzzle.part2(testInput) == puzzle.testResultPart2)
+	var result: Any
+	println(
+		"Part 1 of ${puzzle.name} executed with TEST input in ${
+			measureTimeMillis { result = puzzle.part1(testInput) }
+		}ms with result: $result should be ${puzzle.testResultPart1}"
+	)
+	println(
+		"Part 2 of ${puzzle.name} executed with TEST input in ${
+			measureTimeMillis { result = puzzle.part2(testInput) }
+		}ms with result: $result should be ${puzzle.testResultPart2}"
+	)
 
 	val input = readInput(puzzle.name)
-	var result : Any
-	println("Part 1 of ${puzzle.name} solved in ${measureTimeMillis { result = puzzle.part1(input) }}ms with result: $result")
-	println("Part 2 of ${puzzle.name} solved in ${measureTimeMillis { result = puzzle.part2(input) }}ms with result: $result")
+	println(
+		"Part 1 of ${puzzle.name} executed with REAL input in ${
+			measureTimeMillis { result = puzzle.part1(input) }
+		}ms with result: $result"
+	)
+	println(
+		"Part 2 of ${puzzle.name} executed with REAL input in ${
+			measureTimeMillis { result = puzzle.part2(input) }
+		}ms with result: $result"
+	)
 }
 
 typealias Input = List<String>
